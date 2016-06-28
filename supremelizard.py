f19db84a12afbce5a556c983472321fceadee1b6
allanko <allanko747@gmail.com>
2016-06-28 16:38:55 -0400

    fixing some html entities

--- supremelizard.py
+++ supremelizard.py
@@ -17,13 +17,22 @@ today = datetime.date.today()
 
 stories = mc.storyList(query, solr_filter = [mc.publish_date_query(today - step, today), sources], rows = 1000)
 headlines = [s['title'] for s in stories if 'Trump' in s['title']]
-pick = random.choice(headlines)
-if 'Donald Trump' in pick:
-    tweet = pick.replace('Donald Trump', 'Supreme Lizard')
-elif 'the Trump' in pick:
-    tweet = pick.replace('the Trump', 'the Lizard')
-else:
-    tweet = pick.replace('Trump', 'Supreme Lizard')
+pick = random.choice(headlines).strip()
+
+# catch some common html entities
+pick = pick.replace('&#8216;', "'")
+pick = pick.replace('&#8217;', "'")
+pick = pick.replace('&#038;', "&")
+pick = pick.replace('&#x2018;', "'")
+pick = pick.replace('&#x2019;', "'")
+
+while 'Trump' in pick:
+    if 'Donald Trump' in pick:
+        pick = pick.replace('Donald Trump', 'Supreme Lizard')
+    elif 'the Trump' in pick:
+        pick = pick.replace('the Trump', 'the Lizard')
+    else:
+        pick = pick.replace('Trump', 'Supreme Lizard')
 
 # tweeting
 CONSUMER_KEY = parser.get('TWITTER', 'CONSUMER_KEY')
@@ -34,4 +43,4 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-api.update_status(tweet)
+api.update_status(pick)
